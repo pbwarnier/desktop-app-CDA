92e9c4911323b09192249de3e5ae8025d3b0023a
Pierre-Baptiste Warnier <warnier.pb@gmail.com>
2021-11-06 19:08:35 +0100

    save project

---
+++ src/electron-starter.js
@@ -0,0 +1,41 @@
+const electron = require('electron');
+// Module to control application life.
+const app = electron.app;
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+
+    // and load the index.html of the app.
+    mainWindow.loadURL('http://localhost:3000');
+
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+}
+
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow()
+    }
+});
